2def06d3fdb023e32adeae4d788a7d04911fe66a
zhangzuyuan <1556823221@qq.com>
2021-04-09 20:36:31 +0800

    master

--- listPage/Header.js
+++ listPage/Header.js
@@ -1,9 +1,11 @@
+
+
 class Header extends React.Component{
     render(){
         return(
             <div>
-                <header>
-                    <a href="#" class="logo">LOGO</a>
+                <header className="PageHeader">
+                    <a href="#" className="logo">LOGO</a>
                     <ul>
                         <li><a href="#">主页</a></li>
                         <li><a href="#">主页</a></li>
