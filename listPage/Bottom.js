003f28c31a100419c72aed26692a5d0933b77515
zhangzuyuan <1556823221@qq.com>
2021-04-10 19:29:59 +0800

    master

--- listPage/Bottom.js
+++ listPage/Bottom.js
@@ -8,14 +8,14 @@ class Bottom extends React.Component{
                         <div className="socialMedia">
 
                         </div>
-                        <p className="rightsText">2122222222222222222222222222</p>
+                        <p className="rightsText">本站由山东大学信息化技术中心支持创办，由山东大学OOPS社团运行维护</p>
                     </div>
                     <div className="rightCol">
                         <h1>山东大学镜像站</h1>
                         <div className="border">
                         
                         </div>
-                        <p></p>
+                        <p>可以通过输入相关信息向我们反馈</p>
                         <form className="newsLetterForm">
                             <input type="text" className="txtB"></input>
                             <input type="submit" className="btn" value="submit"></input>
