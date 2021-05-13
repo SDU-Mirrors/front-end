b47617e8b5cd212b0be60e4a165a0e73bc40221f
zhangzuyuan <1556823221@qq.com>
2021-05-14 01:09:37 +0800

    wuh

--- list/src/Components/Layouts/Footer.js
+++ list/src/Components/Layouts/Footer.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
-export default ()=>
-    <div>
-        <h1>Footer</h1>
-    </div>
+
+  
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  }
+}));
+
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+      <AppBar position="fixed" color="primary" className={classes.appBar}>
+        <Toolbar>
+        </Toolbar>
+      </AppBar>
+  );
+}
--- list/src/Components/Layouts/Header.js
+++ list/src/Components/Layouts/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {AppBar,Toolbar, Typography} from '@material-ui/core'
+import {AppBar,Toolbar, Typography,Tabs,Tab} from '@material-ui/core'
 
 export default ()=>
     <AppBar position="static">
         <Toolbar>
             <Typography>
-                
+
             </Typography>
+            
         </Toolbar>
     </AppBar>
