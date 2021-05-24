8277c10317319dcebdfd3702f1378310fe51ce12
zhangzuyuan <1556823221@qq.com>
2021-05-24 09:49:08 +0800

    home

---
+++ home/src/Components/App.js
@@ -0,0 +1,28 @@
+import React,{Fragment} from 'react'
+import {Header,Footer} from './Layouts'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    App: {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+    },
+    main: {
+      marginTop: theme.spacing(8),
+      marginBottom: theme.spacing(2),
+    },
+}));
+
+export default function App(){
+    const classes = useStyles();
+
+    return (
+        <div className={classes.App}>
+            <Fragment>
+                <Header/>
+                <Footer/>
+            </Fragment>
+        </div>
+    )
+}
