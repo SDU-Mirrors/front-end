cd48c873dd53bc390e387a55a7c66b943a605ccd
kvrmnks <tayyx2000@163.com>
2021-07-30 23:21:56 +0800

    change tag styles

--- docs-src/src/components/CategoryList.js
+++ docs-src/src/components/CategoryList.js
@@ -1,17 +1,17 @@
 import React from "react"
 import {Container, ListGroup} from "react-bootstrap"
-
+import "./Content/TagArea.css"
 export default function Catagory(props){
     // console.log(props.tags_list);
     const tags_list = props.tags_list;
     return (
         <Container>
-          <h3>Category</h3>
+          <h4 className="center-text">标签</h4>
           <ListGroup>
           {
             tags_list.map((v, i) => {
                 if(v != null)
-                return (<ListGroup.Item key={i}><a href={'/docs/'+v}>{v}</a></ListGroup.Item>);
+                return (<ListGroup.Item className="tags" key={i}><a href={'/docs/'+v}>{v}</a></ListGroup.Item>);
                 else
                   return "";
             })
