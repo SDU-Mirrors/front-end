5df0d5c6fda4b8ea208bcf5eba23f6cd6a6813e8
hongjx175 <1751215626@qq.com>
2021-08-13 19:22:11 +0800

    修改 Right.css

--- list/src/Components/Mirror/configGeneratorCard.js
+++ list/src/Components/Mirror/configGeneratorCard.js
@@ -5,166 +5,166 @@ import SyntaxHighlighter from "react-syntax-highlighter"
 import {dark} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import {ReactComponent as ConfigSVG} from "./source/配置.svg"
 import {ReactComponent as DownloadedSVG} from "./source/下载.svg";
+import "./Right.css"
 
 /**
  * 配置生成器组件
  */
 export default class ConfigGeneratorCard extends Component {
-  state = {
-    // 配置生成器可见性
-    configGeneratorVisible: false,
-    // 选择发行版
-    selectDistrib: undefined,
-    // 选择的发行版的版本号或版本别名
-    selectVersion: undefined,
-    // 是否显示软件源配置文本块
-    showConfigBlock: false,
-    // 软件源配置文本块
-    configBlock: undefined
-  };
-  myStyle = {
-    buttonStyle: {color: '#9C0C13', borderColor: '#9C0C13'},
-    CascaderStyle: {outlineColor: '#9C0C13'}
-
-  }
-
-  /**
-   * 显示配置生成器对话框
-   */
-  showDownloadForm = () => {
-    this.setState({configGeneratorVisible: true});
-  };
-
-  /**
-   * 退出配置生成器对话框
-   */
-  handleConfigGeneratorCancel = () => {
-    this.setState({configGeneratorVisible: false});
-  };
-
-  /**
-   * 确认选择发行版和版本
-   *
-   * @param selectedOptions 级联选择器各级的值。
-   *        其中[0]为发行版名称，[1]为版本别名
-   */
-  onConfigChange = selectedOptions => {
-    this.setState({
-      selectDistrib: selectedOptions[0],
-      selectVersion: selectedOptions[1]
-    });
-  };
-
-  /**
-   * 生成配置
-   */
-  handleGenerateConfig = () => {
-    let configBlock;
-    switch (this.state.selectDistrib) {
-      case "ubuntu":
-        configBlock = buildUbuntuBlock(this.state.selectVersion);
-        this.setState({showConfigBlock: true, configBlock: configBlock});
-        break;
-      case "debian":
-        configBlock = buildDebianBlock(this.state.selectVersion);
-        this.setState({showConfigBlock: true, configBlock: configBlock});
-        break;
-      case "archlinux":
-        configBlock = buildArchBlock();
-        this.setState({showConfigBlock: true, configBlock: configBlock});
-        break;
-      case "rocky":
-        configBlock = buildRockyBlock();
-        this.setState({showConfigBlock: true, configBlock: configBlock});
-        break;
-      default:
-        break;
+    state = {
+        // 配置生成器可见性
+        configGeneratorVisible: false,
+        // 选择发行版
+        selectDistrib: undefined,
+        // 选择的发行版的版本号或版本别名
+        selectVersion: undefined,
+        // 是否显示软件源配置文本块
+        showConfigBlock: false,
+        // 软件源配置文本块
+        configBlock: undefined
+    };
+    myStyle = {
+        buttonStyle: {color: '#9C0C13', borderColor: '#9C0C13'},
+        CascaderStyle: {outlineColor: '#9C0C13'}
     }
-  };
 
-  render() {
-    return (
-        <div>
-          <h2>配置生成</h2>
-          <p>生成发行版的配置文件</p>
-          <Button
-              type="default"
-              icon={<Icon component={ConfigSVG}/>}
-              onClick={this.showDownloadForm}
-              style={this.myStyle.buttonStyle}
-          >
-            配置生成器
-          </Button>
-          <Modal
-              visible={this.state.configGeneratorVisible}
-              title={"配置生成器"}
-              onCancel={this.handleConfigGeneratorCancel}
-              footer={null}
-              width={800}
-          >
-            <Form layout="vertical">
-              <Form.Item>
-                <Row gutter={8}>
-                  <Col span={20}>
-                    <Cascader
-                        expandTrigger="hover"
-                        placeholder="请选择发行版"
-                        options={this.props.config}
-                        onChange={this.onConfigChange}
-                        autoFocus={<style></style>}
-                    />
-                  </Col>
-                  <Col span={4}>
-                    <Button type="default"
-                            onClick={this.handleGenerateConfig}
-                            icon={<Icon component={ConfigSVG}/>}
-                            style={this.myStyle.buttonStyle}>
-                      生成
-                    </Button>
+    /**
+     * 显示配置生成器对话框
+     */
+    showDownloadForm = () => {
+        this.setState({configGeneratorVisible: true});
+    };
 
-                  </Col>
-                </Row>
-                <Row>
-                  <ConfigBlock
-                      showConfigBlock={this.state.showConfigBlock}
-                      configBlock={this.state.configBlock}
-                  />
-                </Row>
-              </Form.Item>
-            </Form>
-          </Modal>
-        </div>
-    );
-  }
+    /**
+     * 退出配置生成器对话框
+     */
+    handleConfigGeneratorCancel = () => {
+        this.setState({configGeneratorVisible: false});
+    };
+
+    /**
+     * 确认选择发行版和版本
+     *
+     * @param selectedOptions 级联选择器各级的值。
+     *        其中[0]为发行版名称，[1]为版本别名
+     */
+    onConfigChange = selectedOptions => {
+        this.setState({
+            selectDistrib: selectedOptions[0],
+            selectVersion: selectedOptions[1]
+        });
+    };
+
+    /**
+     * 生成配置
+     */
+    handleGenerateConfig = () => {
+        let configBlock;
+        switch (this.state.selectDistrib) {
+            case "ubuntu":
+                configBlock = buildUbuntuBlock(this.state.selectVersion);
+                this.setState({showConfigBlock: true, configBlock: configBlock});
+                break;
+            case "debian":
+                configBlock = buildDebianBlock(this.state.selectVersion);
+                this.setState({showConfigBlock: true, configBlock: configBlock});
+                break;
+            case "archlinux":
+                configBlock = buildArchBlock();
+                this.setState({showConfigBlock: true, configBlock: configBlock});
+                break;
+            case "rocky":
+                configBlock = buildRockyBlock();
+                this.setState({showConfigBlock: true, configBlock: configBlock});
+                break;
+            default:
+                break;
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>配置生成</h2>
+                <p>生成发行版的配置文件</p>
+                <Button
+                    type="default"
+                    icon={<Icon component={ConfigSVG}/>}
+                    onClick={this.showDownloadForm}
+                    style={this.myStyle.buttonStyle}
+                >
+                    配置生成器
+                </Button>
+                <Modal
+                    visible={this.state.configGeneratorVisible}
+                    title={"配置生成器"}
+                    onCancel={this.handleConfigGeneratorCancel}
+                    footer={null}
+                    width={800}
+                >
+                    <Form layout="vertical">
+                        <Form.Item>
+                            <Row gutter={8}>
+                                <Col span={20}>
+                                    <Cascader
+                                        expandTrigger="hover"
+                                        placeholder="请选择发行版"
+                                        options={this.props.config}
+                                        onChange={this.onConfigChange}
+                                        autoFocus={<style></style>}
+                                    />
+                                </Col>
+                                <Col span={4}>
+                                    <Button type="default"
+                                            onClick={this.handleGenerateConfig}
+                                            icon={<Icon component={ConfigSVG}/>}
+                                            style={this.myStyle.buttonStyle}>
+                                        生成
+                                    </Button>
+
+                                </Col>
+                            </Row>
+                            <Row>
+                                <ConfigBlock
+                                    showConfigBlock={this.state.showConfigBlock}
+                                    configBlock={this.state.configBlock}
+                                />
+                            </Row>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+            </div>
+        );
+    }
 }
 
 /**
  * 配置生成器的软件源文本块组件
  */
 class ConfigBlock extends Component {
-  render() {
-    let block = "";
-    if (this.props.showConfigBlock) {
-      // console.log(typeof this.props.configBlock);
-      // block = this.props.configBlock.split("\n").map(function(item, id) {
-      //   return (
-      //     <span key={id}>
-      //       {item}
-      //       <br />
-      //     </span>
-      //   );
-      // });
-      block = this.props.configBlock;
-      block = block.substring(0, block.length - 1);
+    render() {
+        let block = "";
+        if (this.props.showConfigBlock) {
+            // console.log(typeof this.props.configBlock);
+            // block = this.props.configBlock.split("\n").map(function(item, id) {
+            //   return (
+            //     <span key={id}>
+            //       {item}
+            //       <br />
+            //     </span>
+            //   );
+            // });
+            block = this.props.configBlock;
+            block = block.substring(0, block.length - 1);
+        }
+        return (
+            <Col className="config-block" span={24}>
+                <SyntaxHighlighter language="powershell" style={dark}>
+                    {block}
+                </SyntaxHighlighter>
+            </Col>
+        );
     }
-    return (
-        <Col className="config-block" span={24}>
-          <SyntaxHighlighter language="powershell" style={dark}>
-            {block}
-          </SyntaxHighlighter>
-        </Col>
-    );
-  }
 }
 
 /**
@@ -175,12 +175,12 @@ class ConfigBlock extends Component {
  * @returns {string} 返回Ubuntu配置的一行
  */
 function buildUbuntuLine(val, version) {
-  return (
-      val +
-      " https://mirrors.sdu.edu.cn/ubuntu/ " +
-      version +
-      " main restricted universe multiverse\n"
-  );
+    return (
+        val +
+        " https://mirrors.sdu.edu.cn/ubuntu/ " +
+        version +
+        " main restricted universe multiverse\n"
+    );
 }
 
 /**
@@ -190,12 +190,12 @@ function buildUbuntuLine(val, version) {
  * @returns {string} 返回Ubuntu软件源配置的文本块
  */
 function buildUbuntuBlock(version) {
-  return (
-      // "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释\n" +
-      buildUbuntuLine("deb", version) +
-      buildUbuntuLine("deb", version + "-security") +
-      buildUbuntuLine("deb", version + "-updates")
-  );
+    return (
+        // "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释\n" +
+        buildUbuntuLine("deb", version) +
+        buildUbuntuLine("deb", version + "-security") +
+        buildUbuntuLine("deb", version + "-updates")
+    );
 }
 
 /**
@@ -206,13 +206,13 @@ function buildUbuntuBlock(version) {
  * @returns {string} 返回Debian配置的一行
  */
 function buildDebianLine(val, version) {
-  return (
-      val +
-      " https://mirrors.sdu.edu.cn/debian" +
-      version +
-      " main contrib non-free\n"
+    return (
+        val +
+        " https://mirrors.sdu.edu.cn/debian" +
+        version +
+        " main contrib non-free\n"
 
-  );
+    );
 }
 
 /**
@@ -222,12 +222,12 @@ function buildDebianLine(val, version) {
  * @returns {string} 返回Debian软件源配置的文本块
  */
 function buildDebianBlock(version) {
-  return (
-      // "目前还未提供debian-security，请注意添加\n" +
-      buildDebianLine("deb", "") +
-      buildDebianLine("deb", "" + "-security buster/updates") +
-      buildDebianLine("deb", " " + "buster-updates")
-  );
+    return (
+        // "目前还未提供debian-security，请注意添加\n" +
+        buildDebianLine("deb", "") +
+        buildDebianLine("deb", "" + "-security buster/updates") +
+        buildDebianLine("deb", " " + "buster-updates")
+    );
 }
 
 /**
@@ -236,7 +236,7 @@ function buildDebianBlock(version) {
  * @returns {string} 返回ArchLinux软件源配置的文本块
  */
 function buildArchBlock() {
-  return "Server = https://mirrors.sdu.edu.cn/archlinux/$repo/os/$arch ";
+    return "Server = https://mirrors.sdu.edu.cn/archlinux/$repo/os/$arch ";
 }
 
 /**
@@ -245,11 +245,11 @@ function buildArchBlock() {
  * @returns {string} 返回RockyLinux配置命令
  */
 function buildRockyBlock() {
-  return "# 1. 将所有的官方主镜像地址替换为山东大学镜像站地址。\n" +
-      "sed -e 's|^mirrorlist=|#mirrorlist=|g' \\\n" +
-      "    -e 's|^#baseurl=http://dl.rockylinux.org/$contentdir|baseurl=https://mirrors.sdu.edu.cn/rocky|g' \\\n" +
-      "    -i.bak \\\n" +
-      "    /etc/yum.repos.d/Rocky-*.repo\n" +
-      "# 2. 更新缓存。\n" +
-      "     dnf makecache"
+    return "# 1. 将所有的官方主镜像地址替换为山东大学镜像站地址。\n" +
+        "sed -e 's|^mirrorlist=|#mirrorlist=|g' \\\n" +
+        "    -e 's|^#baseurl=http://dl.rockylinux.org/$contentdir|baseurl=https://mirrors.sdu.edu.cn/rocky|g' \\\n" +
+        "    -i.bak \\\n" +
+        "    /etc/yum.repos.d/Rocky-*.repo\n" +
+        "# 2. 更新缓存。\n" +
+        "     dnf makecache"
 }
