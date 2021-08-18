7791ec7dcd38c0112fa5e5e8cc0713edbde5b512
kvrmnks <tayyx2000@163.com>
2021-08-18 17:37:43 +0800

    False ignorecases

---
+++ docs-src/src/components/Pagination.js
@@ -0,0 +1,12 @@
+import { Pagination } from '@material-ui/lab';
+import { Grid } from '@material-ui/core';
+import React from "react"
+export default function Page(props){
+    // console.log(props)
+    // 分页的下边栏
+    return (
+    <Grid  container direction="row" justify="center" alignItems="center">
+        <Pagination count={props.totalPage} defaultPage={props.defaultPage} onChange={props.onChange}/>
+    </Grid>
+  );
+}
