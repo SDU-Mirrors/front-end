75938ae9596683c4b582b52b20877a8d2a6b26a3
icy <i@icys.top>
2021-09-14 22:47:07 +0800

    Update 404.js

--- docs-src/src/pages/404.js
+++ docs-src/src/pages/404.js
@@ -4,9 +4,8 @@ import { graphql } from "gatsby"
 
 const NotFoundPage = ({ data, location }) => {
   // const siteTitle = data.site.siteMetadata.title
-
+  document.title = '404 - 山东大学镜像站';
   return (
-      document.title = '404 - 山东大学镜像站';
       <div>404 not found.</div>
   )
 }
