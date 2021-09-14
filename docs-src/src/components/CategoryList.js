0e85f8893fc720be8661362dd4562a6d20e53b47
icy <i@icys.top>
2021-09-14 23:06:19 +0800

    revert commit

--- docs-src/src/components/CategoryList.js
+++ docs-src/src/components/CategoryList.js
@@ -5,7 +5,7 @@ import "../css/card.css"
 export default function Category(props) {
   // console.log(props.tags_list);
   const tags_list = props.tags_list;
-  document.title = '博客列表 - 山东大学镜像站';
+  // document.title = '博客列表 - 山东大学镜像站';
     return (
     <Container className="docs-category-body">
       <h4 className="center-text">标签</h4>
