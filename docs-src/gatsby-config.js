abf01c2437d35981f409b40951421507e7e6c9be
icy <i@icys.top>
2021-09-17 09:27:44 +0800

    Update gatsby-config.js

--- docs-src/gatsby-config.js
+++ docs-src/gatsby-config.js
@@ -120,7 +120,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `static/favicon.ico`, // This path is relative to the root of the site.
+        icon: `/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
