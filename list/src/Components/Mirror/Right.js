a73f690964b70ce1dbb47453b9ec4e3ca475113b
hongjx175 <1751215626@qq.com>
2022-02-09 17:11:14 +0800

    modify insert

--- list/src/Components/Mirror/Right.js
+++ list/src/Components/Mirror/Right.js
@@ -18,24 +18,24 @@ export default class Right extends Component {
         config: null
     };
 
-    /**
-     * 获取ISO下载链接
-     */
-    fetch_iso_links = () => {
-        this.setState({
-            fetching_slots: true
-        });
-        axios({
-            url: "/static/isoLinks.json",
-            method: "get"
-        }).then(response => {
-            const isoLinks = response.data;
-            console.log("in fetch_iso_links", isoLinks);
-            this.setState({
-                isoLinks: isoLinks
-            });
-        });
-    };
+    /* /!**
+      * 获取ISO下载链接
+      *!/
+     fetch_iso_links = () => {
+         this.setState({
+             fetching_slots: true
+         });
+         axios({
+             url: "/static/isoLinks.json",
+             method: "get"
+         }).then(response => {
+             const isoLinks = response.data;
+             console.log("in fetch_iso_links", isoLinks);
+             this.setState({
+                 isoLinks: isoLinks
+             });
+         });
+     };*/
 
     /**
      * 获取发行版的版本名，及对应的版本号或版本别名
@@ -57,8 +57,7 @@ export default class Right extends Component {
     };
 
     componentDidMount() {
-        this.fetch_iso_links();
-        //console.log(this.state.isoLinks)
+        // this.fetch_iso_links();
         this.fetch_config();
     }
 
@@ -71,9 +70,9 @@ export default class Right extends Component {
                     {/*        <AnnouncementIcon color="secondary"/>新闻公告：*/}
                     {/*    </Typography>*/}
                     {/*</div>*/}
-                    <div className="side-card">
-                        <DownloadFormCard isoLinks={this.state.isoLinks}/>
-                    </div>
+                    {/*<div className="side-card">*/}
+                    {/*    <DownloadFormCard isoLinks={this.state.isoLinks}/>*/}
+                    {/*</div>*/}
                     <div className="side-card">
                         <ConfigGeneratorCard config={this.state.config}/>
                     </div>
